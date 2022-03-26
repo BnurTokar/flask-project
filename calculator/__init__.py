e12852264e68362d6cf225072e1ba42d47d27199
beyzanurtokar <bt268@njit.edu>
2022-03-26 16:08:24 -0400

    Created Calculator class by using strategy design pattern

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,20 +1,34 @@
 """ This is the Calculator Class"""
-
+from operations import Operation, Addition as Add, Subtraction as Sub, Multiplication as Mul, Division as Div, Addition
+from calculation import Calculation
 
 class Calculator:
-    """ This is the default result property"""
-    result = 0
+    """This is Calculator class"""
+    @staticmethod
+    def add (*arg):#arg gets unlimited value for add operation
+        "This is add operation"
+        result = 0
+        for value in arg:
+            calculation = Calculation(result, value)
+            calculation.setStrategy(Add) #calling operations.Addition() class
+            result =+ calculation.get_result()
+        return result
 
-    def add(self, value_1):
-        """ This is the add method"""
-        self.result = self.result + value_1
-        return self.result
+    @staticmethod
+    def sub(value_1,value_2):
+        calculation = Calculation(value_1, value_2 )
+        calculation.setStrategy(Sub) #calling operations.Subtraction() class
+        return calculation.get_result()
 
-    def subtract(self, value_1):
-        """ This is the subtract method"""
-        self.result = self.result - value_1
-        return self.result
+    @staticmethod
+    def mul(value_1, value_2):
+        calculation = Calculation(value_1, value_2)
+        calculation.setStrategy(Mul) #calling operations.Multiplication() class
+        return calculation.get_result()
 
-    def get_result(self):
-        """ This is the get result method"""
-        return self.result
+    @staticmethod
+    def div(value_1, value_2):
+        calculation = Calculation(value_1, value_2)
+        calculation.setStrategy(Div) #calling operations.Division() class
+        return calculation.get_result()
+
