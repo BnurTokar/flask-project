fa990639d14644090759429ad51105f90486d8f8
beyzanurtokar <bt268@njit.edu>
2022-03-26 16:13:10 -0400

    Created Calculator class by using strategy design pattern

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,7 +1,5 @@
 """Testing the Calculator"""
 from calculator import Calculator, Operation
-from calculator.operations import Addition, Subtraction, Multiplication, Division
-
 def test_calculator_is_instance():
     calculator = Calculator()
     assert isinstance(calculator, Calculator)
